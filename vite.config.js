1b9d22db353f368a79f0935785b3fbbe69188cd7
xyxsns80 <441398@qq.com>
2026-02-14 19:07:24 +0800

    Add GitHub Actions deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-export default defineConfig({ plugins: [vue()], base: '/openClawTest001/' })
+
+export default defineConfig({
+  plugins: [vue()],
+  base: '/openClawTest001/',
+})
